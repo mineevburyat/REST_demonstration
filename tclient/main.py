ef175b273030696a692d11413f5392e1cc6d074a
mineevburyat <mineev.buryat@gmail.com>
2022-04-06 13:22:40 +0800

    fastapi заглушки

--- tclient/main.py
+++ tclient/main.py
@@ -1,57 +1,49 @@
 from fastapi import FastAPI, Path
 import uvicorn
-from tclient.shema import CPUPercent, PartInfo, CPUTimesPercent
-from tserver.monitoring import *
-from calcpicelery.tasks import calcPi
+from shema import CPUPercent, PartInfo, CPUTimesPercent
+
+
 import json
 import datetime
 import os
 
 app = FastAPI()
 
-hostconf = getInfo()
-@app.get('/', response_model=CommonInfo)
+#заглушка
+result = '{"id":"test-fcff2b31-f6a5-4c28-8e87-2a530b4cd2c4", "status": "SUCCESS", "pushqtime": "Thu Feb 17 00:47:22 2022", "starttime": "Thu Feb 17 00:47:22 2022", "result": 3.14159, "stoptime": "Thu Feb 17 00:49:08 2022"}'
+
+@app.get('/',)
 async def home():
-    return updateInfo(hostconf)
+    return "show info"
 
 @app.get('/leibnic/{decimal}')
 async def leibnic_method_calc_from_celery(decimal: int):
-    result = calcPi.delay(decimal)
-    selfresult = {'status': "WAITING", 'pushqtime': datetime.datetime.now().strftime('%c')}
-    with open('results/'+result.id, 'w') as f:
-        json.dump(selfresult, f)
-    return {'id': result.id}
+    #запуск клиента thrift и запрос на RPC calcPI в ответ получаем json result задачи
+    # result = calcPi.delay(decimal)
+    return result
 
 @app.get("/tasks/{task_id}")
 async def get_status_task(task_id: str):
-    try:
-        with open('results/' + task_id, 'rb') as f:
-            fresults = json.load(f)
-    except FileNotFoundError:
-        return {'error': "tasks not found"}
-    return {**fresults}
+    #запуск клиента thrift и запрос на RPC calcPI в ответ получаем json result задачи
+    # result = calcPi.delay(decimal)
+    return result
 
 @app.get('/alltasks/')
 async def get_tasks_list():
-    files = os.listdir('results')
-    results = {}
-    for file in files:
-        with open('results/'+file, 'r') as f:
-            results[file] = json.load(f)
-    return results
+    return "list of tasks"
 
 
-@app.get('/cpu/times_percent', response_model=Dict[int, CPUTimesPercent])
+@app.get('/cpu/times_percent')
 async def cpu_times_percent():
-    return getCPUTimesPercent()
+    return "getCPUTimesPercent()"
 
-@app.get('/cpu/percent', response_model=CPUPercent)
+@app.get('/cpu/percent')
 async def cpu_percent():
-    return getCPUPercent()
+    return "getCPUPercent()"
 
-@app.get('/partitions/usage', response_model=List[PartInfo])
+@app.get('/partitions/usage')
 async def partition_usage():
-    return getPartitionUsage()
+    return "getPartitionUsage()"
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
