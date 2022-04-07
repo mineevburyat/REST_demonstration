05833a74d5a82e51591da97690cd59173b0f6b73
mineevburyat <mineev.buryat@gmail.com>
2022-04-08 02:39:24 +0800

    test fastapi with tclient

--- tclientfolder/main.py
+++ tclientfolder/main.py
@@ -18,31 +18,19 @@ async def calc_pi_in_celery(decimal: int):
     # запуск клиента thrift, который передаст RPC на thrift server и получит uuid задачи
     # result = calcPi.delay(decimal)
     result = startCalcPi(decimal)
-    return '{"id": "{result}"}'.format(result=result)
+    return result
 
 @app.get("/task/{task_id}")
 async def get_status_task(task_id: str):
     # запуск клиента thrift, который передаст RPC на thrift server и получит result задачи с заданным uuid
     # result = calcPi.delay(decimal)
     result = getTaskStatus(task_id)
-    return resultTask(**result)
+    # print(result)
+    return result
 
 @app.get('/alltasks/')
 async def get_tasks_list():
-    return "list of tasks"
-
-
-@app.get('/cpu/times_percent')
-async def cpu_times_percent():
-    return "getCPUTimesPercent()"
-
-@app.get('/cpu/percent')
-async def cpu_percent():
-    return "getCPUPercent()"
-
-@app.get('/partitions/usage')
-async def partition_usage():
-    return "getPartitionUsage()"
+    return listTask()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, log_level="info")
